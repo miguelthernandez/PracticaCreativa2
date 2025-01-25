927eae65b4f0d41212cc790b58976ce57d2b825a
miguelthernandez <miguel.tenedor8@gmail.com>
2025-01-25 16:46:09 +0100

    cambios docker

--- ProductPage/apartado2.py
+++ ProductPage/apartado2.py
@@ -9,12 +9,12 @@ call(['sudo', 'apt-get', 'install', '-y', 'docker.io'])
 call(['sudo', 'apt-get', 'install', '-y', 'docker-compose'])
 
 # Eliminamos el contenedor y la imagen si lo hubiera 
-#os.system('sudo docker stop 10-product-page')
-#os.system('sudo docker rm 10-product-page')
-#os.system('sudo docker rmi 10/product-page')
+os.system('sudo docker stop product-page-g15')
+os.system('sudo docker rm product-page-g15')
+os.system('sudo docker rmi product-page/g15')
 
 #Crea la imagen con nombre 10/product-page
 call(['sudo','docker', 'build', '-t', 'product-page/g15', '.'])
 
 #Inicia un contenedor llamado "g10-product-page" a partir de la imagen "10/product-page"
-os.system('sudo docker run --name product-page-g15 -p 5080:5080 -e GRUPO_NUMERO=15 -d product-page/g15')
+os.system('sudo docker run --name product-page-g15 -p 9080:5080 -e GRUPO_NUMERO=10 -d 10/product-page')
