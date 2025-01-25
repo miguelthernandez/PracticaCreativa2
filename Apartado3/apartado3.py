487bc862ed94c00899b92a5a0b1b56698591e9be
miguelthernandez <miguel.tenedor8@gmail.com>
2025-01-25 20:47:18 +0100

    correccion apartado3

--- Apartado3/apartado3.py
+++ Apartado3/apartado3.py
@@ -1,25 +1,38 @@
-#!usr/bin/python3
 from subprocess import call
-import os
-
-#Instalamos Docker
-call(['sudo', 'apt-get', 'install', '-y', 'docker.io'])
-call(['sudo', 'apt-get', 'install', '-y', 'docker-compose'])
-
-#Instalaciones en la máquina virtual
-call(['sudo', 'apt-get', 'upgrate'])
-call(['sudo', 'apt-get', 'install', '-y', 'python3-pip']) 
-call(['sudo', 'apt-get', 'install', '-y','git'])
-call(['git', 'clone', 'https://github.com/CDPS-ETSIT/practica_creativa2.git'])
-call(['sudo', 'apt-get', 'update'])
-
-
-#Ejecutar el comando que dice el enunciado en la ruta src/reviews:				
-os.chdir('practica_creativa2/bookinfo/src/reviews')
-os.system('sudo docker run --rm -u root -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build')
-os.chdir(os.path.expanduser("~"))
-
-#Situarnos en la carpeta donde se encuentra docker-compose.yaml e iniciar los servicios definidos en ese archivo
-#os.chdir('practica_creativa2/bookinfo/src')
-call(['sudo', 'docker-compose', 'build'])
-call(['sudo', 'docker-compose', 'up', '-d'])
+import os, sys
+def build():
+ # Clonar el repositorio
+ os.system('git clone https://github.com/CDPS-ETSIT/practica_creativa2.git')
+ os.system('sudo apt install -y docker-compose')
+ # Construir la aplicación Reviews
+ os.chdir('practica_creativa2/bookinfo/src/reviews')
+ os.system('docker run --rm -u root -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build')
+ # Cambiar de directorio al proyecto principal
+ os.chdir('/home/rrjorge8/PraCreativa2')
+ os.system('pwd') # Mostrar la ruta actual para verificación
+ # Ejecutar los comandos Docker Compose
+ os.system('sudo docker-compose -f docker-compose.yaml build')
+ os.system('sudo docker-compose -f docker-compose.yaml up')
+def start():
+    os.system('sudo docker-compose -f docker-compose.yaml up')
+def startdetached():
+    os.system('sudo docker-compose -f docker-compose.yaml up -d')
+def stop():
+    os.system('sudo docker-compose -f docker-compose.yaml stop')
+def delete():
+    os.system('sudo docker-compose -f docker-compose.yaml down')
+    os.system('sudo rm -rf practica_creativa2/')
+param = sys.argv
+ # Comandos del script
+if param[1] == "build":
+ build()
+elif param[1] == "start":
+ start()
+elif param[1] == "startdetached":
+ startdetached()
+elif param[1] == "stop":
+    stop()
+elif param[1] == "delete":
+ delete()
+else:
+ print("Unknown command")
