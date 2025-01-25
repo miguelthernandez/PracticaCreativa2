cadc377706fc2fef0c8ee633b2322f648ed345a2
miguelthernandez <miguel.tenedor8@gmail.com>
2025-01-25 20:39:27 +0100

    apartado3

--- Apartado2/apartado2.py
+++ Apartado2/apartado2.py
@@ -13,8 +13,8 @@ os.system('sudo docker stop product-page-g15')
 os.system('sudo docker rm product-page-g15')
 os.system('sudo docker rmi product-page/g15')
 
-#Crea la imagen con nombre 10/product-page
+#Crea la imagen con nombre 15/product-page
 call(['sudo','docker', 'build', '-t', 'product-page/g15', '.'])
 
-#Inicia un contenedor llamado "g10-product-page" a partir de la imagen "10/product-page"
+#Inicia un contenedor llamado "g15-product-page" a partir de la imagen "15/product-page"
 os.system('sudo docker run --name product-page-g15 -p 9080:5080 -e GROUP_NUM=15 -d product-page/g15')
