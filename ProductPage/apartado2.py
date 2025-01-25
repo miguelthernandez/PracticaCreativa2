494fc2579bd6acc9a5239eeb217a9a3bd481a609
miguelthernandez <miguel.tenedor8@gmail.com>
2025-01-25 16:21:13 +0100

    cambio en carpetas apartado2

---
+++ ProductPage/apartado2.py
@@ -0,0 +1,20 @@
+#!usr/bin/python3
+from subprocess import call
+import os
+
+# Instalamos Docker
+call(['sudo', 'apt-get', 'update'])
+call(['sudo', 'apt-get', 'upgrate'])
+call(['sudo', 'apt-get', 'install', '-y', 'docker.io'])
+call(['sudo', 'apt-get', 'install', '-y', 'docker-compose'])
+
+# Eliminamos el contenedor y la imagen si lo hubiera 
+#os.system('sudo docker stop 10-product-page')
+#os.system('sudo docker rm 10-product-page')
+#os.system('sudo docker rmi 10/product-page')
+
+#Crea la imagen con nombre 10/product-page
+call(['sudo','docker', 'build', '-t', 'product-page/g15', '.'])
+
+#Inicia un contenedor llamado "g10-product-page" a partir de la imagen "10/product-page"
+os.system('sudo docker run --name product-page-g15 -p 9080:9080 -e GRUPO_NUMERO=15 -d product-page/g15')
